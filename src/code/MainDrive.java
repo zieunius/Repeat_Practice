fb2a75fd89003cb485a1fa5ae0e7cb1a3e8204f7
강지은 <zieunius@gmail.com>
2019-01-26 11:47:33 +0900

    3.1)별찍기 기초 정답

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -64,6 +64,16 @@ public class MainDrive {
 //		*****
 //		*****
 		
+//		세로 줄바꿈
+		for(int i=0;i<5;i++){
+//			가로 별찍기
+			for(int j=0;j<5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
 		
 		
 		
